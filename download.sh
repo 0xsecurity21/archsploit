92a9f47cddcc4682701101848f610295fc79426f
NeosLab <secure@neoslab.com>
2020-06-02 00:36:59 +0200

    Update Project Core

--- download.sh
+++ download.sh
@@ -7,8 +7,6 @@ rm -f archlinux.sh
 ## Download Script and Configuration File
 wget https://raw.githubusercontent.com/archsploit/archsploit/master/archsploit.conf
 wget https://raw.githubusercontent.com/archsploit/archsploit/master/archsploit.sh
-wget https://raw.githubusercontent.com/archsploit/archsploit/master/deploy.sh
 
 ## Change User Permission
 chmod +x archsploit.sh
-chmod +x deploy.sh
